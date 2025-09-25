3d62c0965b8553e789b084423ce70d18d3a00ac2
marcosbji <marcos-mpcl@hotmail.com>
2025-09-25 20:42:37 -0300

    Finalizado

--- script.js
+++ script.js
@@ -65,3 +65,13 @@ input.addEventListener("keyup", function (event) {
     btn_add.click();
   }
 });
+
+//toggleMode
+function toggleMode(){
+  const html = document.documentElement
+  if(html.classList. contains('light')) {
+    html.classList.remove('light')
+  } else {
+    html.classList.add('light')
+  }
+}
